361f88da9819c377dc85a81b526b6140ee3b895a
zhaozi <zhaocius@gmail.com>
2019-08-09 17:46:25 +0800

    add hauserobber2

--- src/leetcode/LT213_HauseRobber2.java
+++ src/leetcode/LT213_HauseRobber2.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-public class LT198_HauseRobber {
+public class LT213_HauseRobber2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,13 +10,21 @@ public class LT198_HauseRobber {
 	
 	public static int get(int[] nums){
 		int num=nums.length;
-		int[] dp=new int[num];
+		int[] dp=new int[num-1];
 		dp[0]=nums[0];
 		dp[1]=Math.max(nums[0], nums[1]);
-		for(int i=2;i<num;i++){
+		for(int i=2;i<num-1;i++){
 			dp[i]=Math.max(dp[i-1], dp[i-2]+nums[i]);
 		}
-		return dp[num-1];
+		
+		int[] dp1=new int[num-1];
+		dp[0]=nums[1];
+		dp[1]=Math.max(nums[1], nums[2]);
+		for(int i=2;i<num-1;i++){
+			dp[i]=Math.max(dp[i-1], dp[i-2]+nums[i+1]);
+		}
+		
+		return dp[num-2]>dp1[num-2]?dp[num-2]:dp1[num-2];
 	}
-	
+
 }
