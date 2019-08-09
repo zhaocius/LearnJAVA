e64bea6b24f830f5cedc66455e04832a5bae0413
zhaozi <zhaocius@gmail.com>
2019-08-09 17:20:59 +0800

    add hauserobber

---
+++ src/leetcode/LT198_HauseRobber.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class LT198_HauseRobber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums=new int[]{1,2,3,4,5};
+		System.out.println(get(nums));
+	}
+	
+	public static int get(int[] nums){
+		int num=nums.length;
+		int[] dp=new int[num];
+		dp[0]=nums[0];
+		dp[1]=Math.max(nums[0], nums[1]);
+		for(int i=2;i<num;i++){
+			dp[i]=Math.max(dp[i-1], dp[i-2]+nums[i]);
+		}
+		return dp[num-1];
+	}
+	
+}
