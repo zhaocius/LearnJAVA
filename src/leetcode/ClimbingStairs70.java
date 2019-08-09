00d591403dcb6cfad0aa32fe5c181479025f72f9
zhaozi <zhaocius@gmail.com>
2019-08-09 16:48:00 +0800

    add climbingstairs

---
+++ src/leetcode/ClimbingStairs70.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class ClimbingStairs70 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println(get(3));
+	}
+
+	public static int get(int stairs){
+		int[] dp=new int[stairs];
+		dp[0]=1;
+		dp[1]=2;
+		for(int i=2;i<stairs;i++){
+			dp[i]=dp[i-1]+dp[i-2];
+		}
+		return dp[stairs-1];
+		
+	}
+
+}
