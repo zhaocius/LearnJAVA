e804ec85666ae3e721e31fca320191bd454f65a7
赵子 <zhaozi@megvii.com>
2021-07-13 10:07:09 +0800

    tes

--- src/leetcode/search/LT257_BinaryTreePaths.java
+++ src/leetcode/search/LT257_BinaryTreePaths.java
@@ -1,24 +1,26 @@
 package leetcode.search;
 
+import algorithm4thedition.BinarySearchTree;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LT257_BinaryTreePaths {
-    public static List<String> binaryTreePaths(Node root) {
+    public static List<String> binaryTreePaths(BinarySearchTree.Node root) {
         List<String> ret = new ArrayList();
         if(root == null) return ret;
         dfs(root, "", ret);
         return ret;
     }
 
-    public static void dfs(Node root,String text ,List<String> ret){
+    public static void dfs(BinarySearchTree.Node root,String text ,List<String> ret){
         if(root == null)
             return;
         if(root.left == null && root.right == null){
-            ret.add(text+root.item);
+            ret.add(text+root.value);
             return;
         }
-        text+=root.item;
+        text+=root.value;
         dfs(root.left,text,ret);
         dfs(root.right,text,ret);
 
--- src/leetcode/tree/LT101_Symmetric_Tree.java
+++ src/leetcode/tree/LT101_Symmetric_Tree.java
@@ -1,5 +1,5 @@
 package leetcode.tree;
-
+import algorithm4thedition.BinarySearchTree;
 public class LT101_Symmetric_Tree{
 /*
     1
@@ -10,17 +10,16 @@ public class LT101_Symmetric_Tree{
 
 */
 
-    /*
-public boolean isSymmetric(TreeNode root) {
+public boolean isSymmetric(BinarySearchTree.Node root) {
     if(root == null) return true;
     return isSymmetric(root.left, root.right);
 }
 
-private boolean isSymmetric(TreeNode t1, TreeNode t2){
+private boolean isSymmetric(BinarySearchTree.Node t1, BinarySearchTree.Node t2){
     if(t1 == null && t2 == null) return true;
     if(t1 == null || t2 == null) return false;
-    if(t1.val != t2.val) return false;
+    if(t1.value != t2.value) return false;
     return isSymmetric(t1.left, t2.right) && isSymmetric(t1.right, t2.left);
 }
-*/
+
 }
